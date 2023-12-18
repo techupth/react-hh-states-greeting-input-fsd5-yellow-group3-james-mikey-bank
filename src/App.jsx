fee9e2577e9910127512d5aa6fe3d23f2ae197d6
acorriaz <mikey.swx@gmail.com>
2023-12-18 21:47:16 +0700

    add ex2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+  const [formData, setFormData] = useState("");
+
+  console.log(formData);
+
+  function handleClick() {
+    setGreetingMessage(formData);
+    setFormData("");
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={formData}
+          onChange={(e) => setFormData(e.target.value)}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleClick}>Update text</button>
       </div>
     </div>
   );
