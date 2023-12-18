c34a0a95b99fe76231eca226aad0f01be1dc2b51
lacitadelle <jkomala99@gmail.com>
2023-12-18 22:10:37 +0700

    implemented react input

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("");
+  const [eventualMessage, setEventualMessage] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{eventualMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={greetingMessage}
+          onChange={(e) => setGreetingMessage(e.target.value)}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={() => setEventualMessage(greetingMessage)}>
+          Update text
+        </button>
       </div>
     </div>
   );
